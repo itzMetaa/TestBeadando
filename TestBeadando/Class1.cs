1c5a316239de44e4c993aa20fe077e4bb23efed4
itzMetaa <bajkaipatrik4@gmail.com>
2020-03-04 21:42:34 +0100

    Add project files.

---
+++ TestBeadando/Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace TestBeadando
+{
+    class Eloadas
+    {
+        private bool[,] foglalasok;
+
+        public bool[,] Foglalasok { get => foglalasok; set => foglalasok = value; }
+
+        public Eloadas(int sor, int hely)
+        {
+            if (sor <= 0 || hely <= 0)
+            {
+                throw new ArgumentException("valami elromlott");
+            }
+            this.Foglalasok = new bool[sor, hely];
+        }
+
+        public bool Foglal()
+        {
+            bool lefoglalt = true;
+            for (int i = 0; i < foglalasok.GetLength(0); i++)
+            {
+                for (int j = 0; j < foglalasok.GetLength(1); j++)
+                {
+                    if (foglalasok[i, j] == false && lefoglalt)
+                    {
+                        lefoglalt = false;
+                        foglalasok[i, j] = true;
+                        break;
+                    }
+                    if (!lefoglalt) break;
+                }
+            }
+            return !lefoglalt ? true : false;
+        }
+
+        public int SzabadHelyek
+        {
+            get
+            {
+                int count = 0;
+                foreach (var item in this.Foglalasok)
+                {
+                    if (item == false)
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            }
+        }
+
+        public bool Teli()
+        {
+            return this.SzabadHelyek == 0 ? true : false;
+        }
+
+        public bool Foglalt(int sorSzam, int helySzam)
+        {
+            if (sorSzam < 1 || helySzam < 1)
+                throw new ArgumentException("valami elromlott");
+            return this.Foglalasok[sorSzam - 1, helySzam - 1];
+        }
+    }
+}
---
+++ TestBeadando/Test.cs
@@ -0,0 +1,114 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestBeadando
+{
+    [TestFixture]
+    class Test
+    {
+        [TestCase]
+        public void Eloadas()
+        {
+            Eloadas eloadas = new Eloadas(5, 5);
+            Assert.AreEqual(new bool[5, 5], eloadas.Foglalasok);
+        }
+        [TestCase]
+        public void EloadasRossz()
+        {
+            Assert.Throws<ArgumentException>(() => {
+                Eloadas eloadas = new Eloadas(-1, -1);
+            });
+        }
+
+        [TestCase]
+        public void Foglal()
+        {
+            Eloadas eloadas = new Eloadas(1, 1);
+            eloadas.Foglal();
+            bool[,] expected = new bool[1, 1];
+            expected[0, 0] = true;
+            Assert.AreEqual(expected, eloadas.Foglalasok);
+        }
+        [TestCase]
+        public void FoglalRossz()
+        {
+            Eloadas eloadas = new Eloadas(1, 1);
+            eloadas.Foglal();
+            bool[,] expected = new bool[1, 1];
+            expected[0, 0] = true;
+            Assert.AreEqual(expected, eloadas.Foglalasok);
+        }
+        [TestCase]
+        public void Szabad()
+        {
+            Eloadas eloadas = new Eloadas(2, 2);
+            Assert.AreEqual(4, eloadas.SzabadHelyek);
+        }
+        [TestCase]
+        public void SzabadFoglal()
+        {
+            Eloadas eloadas = new Eloadas(2, 2);
+            eloadas.Foglal();
+            Assert.AreEqual(3, eloadas.SzabadHelyek);
+        }
+        [TestCase]
+        public void SzabadRossz()
+        {
+            Eloadas eloadas = new Eloadas(2, 2);
+            eloadas.Foglal();
+            eloadas.Foglal();
+            eloadas.Foglal();
+            eloadas.Foglal();
+            eloadas.Foglal();
+            eloadas.Foglal();
+            eloadas.Foglal();
+            eloadas.Foglal();
+            Assert.AreEqual(0, eloadas.SzabadHelyek);
+        }
+        [TestCase]
+        public void Tele()
+        {
+            Eloadas eloadas = new Eloadas(2, 2);
+            eloadas.Foglal();
+            eloadas.Foglal();
+            eloadas.Foglal();
+            eloadas.Foglal();
+            eloadas.Foglal();
+            eloadas.Foglal();
+            eloadas.Foglal();
+            Assert.AreEqual(true, eloadas.Teli());
+        }
+        [TestCase]
+        public void NemTele()
+        {
+            Eloadas eloadas = new Eloadas(2, 2);
+            eloadas.Foglal();
+            Assert.AreEqual(false, eloadas.Teli());
+        }
+        [TestCase]
+        public void NemFoglalt()
+        {
+            Eloadas eloadas = new Eloadas(2, 2);
+            eloadas.Foglal();
+            Assert.AreEqual(false, eloadas.Foglalt(2, 2));
+        }
+        [TestCase]
+        public void Foglalt()
+        {
+            Eloadas eloadas = new Eloadas(2, 2);
+            eloadas.Foglal();
+            Assert.AreEqual(true, eloadas.Foglalt(1, 1));
+        }
+        [TestCase]
+        public void FoglaltRossz()
+        {
+            Eloadas eloadas = new Eloadas(2, 2);
+            eloadas.Foglal();
+            Assert.Throws<ArgumentException>(() => {
+                eloadas.Foglalt(-1, -1);
+            });
+        }
+    }
+}
